67e0087686e19bb8872372f2c1cc8a12714f0410
michaeljosephwatson <michaeljosephwatson@yahoo.com>
2025-12-23 12:46:47 +0000

    Added basic story prototype

--- client/script.js
+++ client/script.js
@@ -2,70 +2,132 @@
 
 // This event listener waits for the entire HTML page to load before running any code
 document.addEventListener("DOMContentLoaded", function () {
+  // Hide all other sections
+  document.getElementById("accessibility-page").hidden = true;
+  document.getElementById("first-scene").hidden = true;
+  document.getElementById("second-scene").hidden = true;
+  document.getElementById("first-scene-ending").hidden = true;
+  document.getElementById("second-scene-ending").hidden = true;
+  document.getElementById("third-scene").hidden = true;
+  document.getElementById("third-scene-ending").hidden = true;
+  document.getElementById("fourth-scene").hidden = true;
+  document.getElementById("restart-button").hidden = true;
+
   console.log("Page loaded!");
 
-  // We find the button using its unique ID from the HTML
-  const ctaButton = document.getElementById("cta-button");
-  const homePage = document.getElementById("homePage");
-  const mainPage = document.getElementById("mainPage");
+  let currentAccessiblityLink = "";
 
-  // Check if the button exists on the page before adding an event listener
+  const ctaButton = document.getElementById("start-button");
   if (ctaButton) {
-    // This function runs whenever the button is clicked
     ctaButton.addEventListener("click", function () {
-//      console.log("CTA button clicked");
-//      alert("Welcome! This is your starting point.");
-      var userName = getUserName();
-      changePageView();
+      console.log("Journey began");
+      document.getElementById("home-page").hidden = true;
+      document.getElementById("accessibility-page").hidden = false;
     });
   }
-  function changePageView() {
-    homePage.classList.add("hide")
-    mainPage.classList.remove("hide")
-  }
-  function getUserName() {
-    var userName = document.getElementById("userName").value;
-    console.log("User name: userName");
-    return userName;
-  }
-  const gameStateTitle = document.getElementById("gameStateTitle");
-  const gameStateText = document.getElementById("gameStateText");
-  const buttonDiv = document.getElementById("buttonDiv");
-  
-  const buttonOne = document.getElementById("buttonOne");
-  const buttonTwo = document.getElementById("buttonTwo");
-  const buttonThree = document.getElementById("buttonThree");
-  
 
-
-  buttonOne.addEventListener("click", function () {
-    buttonDiv.classList.add("hide");
-  });
-
-
-
-  if (buttonOne) {
-    buttonOne.addEventListener("click", function () {
-      buttonDiv.classList.add("hide");
-      gameStateTitle.textContent = "You chose kindness";
-      gameStateText.textContent =
-      "Your kindness changes the situation for the better.";
+  const keyboardAccessibleBtn = document.getElementById(
+    "set-keyboard-accessible"
+  );
+  if (keyboardAccessibleBtn) {
+    keyboardAccessibleBtn.addEventListener("click", function () {
+      console.log("Set normal keyboard accessibility");
+      document.getElementById("accessibility-page").hidden = true;
+      document.getElementById("first-scene").hidden = false;
     });
   }
-  if (buttonTwo) {
-    buttonTwo.addEventListener("click", function () {
-      buttonDiv.classList.add("hide");
-      gameStateTitle.textContent = "You chose to be Neutral";
-      gameStateText.textContent =
-      "Your Neutralness doesn't change the situation.";
+  const motionAccessibleBtn = document.getElementById("set-motion-accessible");
+  if (motionAccessibleBtn) {
+    motionAccessibleBtn.addEventListener("click", function () {
+      console.log("Set motion accessibility");
+      currentAccessibilityLink = ".../accessibility/motion";
+      document.getElementById("accessibility-page").hidden = true;
+      document.getElementById("first-scene").hidden = false;
     });
   }
-  if (buttonThree) {
-    buttonThree.addEventListener("click", function () {
-      buttonDiv.classList.add("hide");
-      gameStateTitle.textContent = "You chose to be mean";
-      gameStateText.textContent =
-      "Your choice changed the situation for the worse.";
+
+  const audioAccessibleBtn = document.getElementById("set-audio-accessible");
+  if (audioAccessibleBtn) {
+    audioAccessibleBtn.addEventListener("click", function () {
+      console.log("Set audio accessibility");
+      currentAccessibilityLink = ".../accessibility/audio";
+      document.getElementById("accessibility-page").hidden = true;
+      document.getElementById("first-scene").hidden = false;
+    });
+  }
+
+  const acceptSproutsBtn = document.getElementById("accept-new-sprouts");
+  if (acceptSproutsBtn) {
+    acceptSproutsBtn.addEventListener("click", function () {
+      console.log("User selected option 1");
+      document.getElementById("first-scene").hidden = true;
+      document.getElementById("second-scene").hidden = false;
+    });
+  }
+
+  const rejectSproutsBtn = document.getElementById("reject-new-sprouts");
+  if (rejectSproutsBtn) {
+    rejectSproutsBtn.addEventListener("click", function () {
+      console.log("User selected option 2");
+      document.getElementById("first-scene").hidden = true;
+      document.getElementById("first-scene-ending").hidden = false;
+      document.getElementById('restart-button').hidden = false;
+    });
+  }
+
+  const acceptBugsBtn = document.getElementById("accept-new-bugs");
+  if (acceptBugsBtn) {
+    acceptBugsBtn.addEventListener("click", function () {
+      console.log("User selected option 1");
+      document.getElementById("second-scene").hidden = true;
+      document.getElementById("third-scene").hidden = false;
+    });
+  }
+
+  const rejectBugsBtn = document.getElementById("reject-new-bugs");
+  if (rejectBugsBtn) {
+    rejectBugsBtn.addEventListener("click", function () {
+      console.log("User selected option 2");
+      document.getElementById("second-scene").hidden = true;
+      document.getElementById("second-scene-ending").hidden = false;
+      document.getElementById('restart-button').hidden = false;
+    });
+  }
+
+  const acceptFaunaBtn = document.getElementById("accept-fauna");
+  if (acceptFaunaBtn) {
+    acceptFaunaBtn.addEventListener("click", function () {
+      console.log("User selected option 1");
+      document.getElementById("third-scene").hidden = true;
+      document.getElementById("fourth-scene").hidden = false;
+      document.getElementById('restart-button').hidden = false;
+    });
+  }
+
+  const rejectFaunaBtn = document.getElementById("reject-fauna");
+  if (rejectFaunaBtn) {
+    rejectFaunaBtn.addEventListener("click", function () {
+      console.log("User selected option 2");
+      document.getElementById("third-scene").hidden = true;
+      document.getElementById("third-scene-ending").hidden = false;
+      document.getElementById('restart-button').hidden = false;
+    });
+  }
+
+  const restartButton = document.getElementById("restart-button");
+  if (restartButton) {
+    restartButton.addEventListener("click", function () {
+      console.log("Restart game");
+      document.getElementById("home-page").hidden = false;
+      document.getElementById("accessibility-page").hidden = true;
+      document.getElementById("first-scene").hidden = true;
+      document.getElementById("second-scene").hidden = true;
+      document.getElementById("first-scene-ending").hidden = true;
+      document.getElementById("second-scene-ending").hidden = true;
+      document.getElementById("third-scene").hidden = true;
+      document.getElementById("third-scene-ending").hidden = true;
+      document.getElementById("fourth-scene").hidden = true;
+      document.getElementById("restart-button").hidden = true;
     });
   }
 });
